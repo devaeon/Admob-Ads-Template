352176df83ece010b280109fa8149f9fa4fe2873
Muzammil Hussain <muzammil@galixo.ai>
2025-01-01 16:37:41 +0500

    first commit

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.devaeon.gradle.convention"
+
+// Configure the build-logic plugins to target JDK 17
+// This matches the JDK used to build the project, and is not related to what is running on device.
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "com.devaeon.gradle.android.androidApplication"
+            implementationClass = "com.devaeon.gradle.convention.AndroidApplicationConventionPlugin"
+        }
+    }
+
+}
